1f11abdfd2c91d0bf9e4b649af442555880291e6
ali <yuanzhe115@gmail.com>
2015-07-18 22:40:49 +0800

    Add a ToolBar instead of ActionBar.

--- app/src/main/java/per/ali/prac/newdesignsuptry/MainActivity.java
+++ app/src/main/java/per/ali/prac/newdesignsuptry/MainActivity.java
@@ -7,50 +7,79 @@ import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.TextInputLayout;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    //        获取DrawerLayout和直接获取NavigationView布局
+    DrawerLayout mDrawer;
+    NavigationView nvContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.design_drawer_main);
+        setContentView(R.layout.drawer_with_main);
+
+        setupToobBar();
+        initNavigationDrawer();
+        initMainPage();
+
+    }
+
+    /**
+     * 初始化抽屉
+     */
+    private void initNavigationDrawer() {
+        //        获取DrawerLayout和直接获取NavigationView布局
+        mDrawer = (DrawerLayout) findViewById(R.id.ly_drawer);
+        nvContent = (NavigationView) findViewById(R.id.nv_main);
 
 /*//        获取NavigationView的FrameLayout布局，**不能达到滑出效果**
         final FrameLayout famNvContent = (FrameLayout) findViewById(R.id.frame_nv_content);*/
 
-//        获取DrawerLayout和直接获取NavigationView布局
-        final DrawerLayout mDrawer = (DrawerLayout) findViewById(R.id.ly_drawer);
-        final NavigationView nvContent = (NavigationView) findViewById(R.id.nv_main);
-        //        动态添加Item和SubMenu item
-//        final SubMenu nvSubMenu  = (SubMenu) findViewById(R.id.nv_item_with_sub);
-//        nvSubMenu.add(R.string.nv_sub_item_run);
+/*//        动态添加Item和SubMenu item
+        final SubMenu nvSubMenu  = (SubMenu) findViewById(R.id.nv_item_with_sub);
+        nvSubMenu.add(R.string.nv_sub_item_run);*/
+
+//        在NavigationView的Menu中动态添加Item
         final Menu nvMenu = nvContent.getMenu();
         nvMenu.add(R.string.nv_item_run);
 //        nvMenu.getItem(3).setCheckable(true);
         nvContent.setNavigationItemSelectedListener(
                 new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(MenuItem menuItem) {
-                menuItem.setChecked(true);
-                mDrawer.closeDrawers();
-                return true;
-            }
-        });
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        mDrawer.closeDrawers();
+                        return true;
+                    }
+                });
+    }
 
-//        利用Toolbar添加DrawerToggle
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar)
+    /**
+     * 初始化主页面
+     */
+    private void initMainPage() {
 
+//        获取TextInputLayout
+        TextInputLayout etEmail = (TextInputLayout) findViewById(R.id.input_layout_email);
+//        下面前两个属性设置后不会自动聚焦
+        etEmail.setFocusable(true);
+        etEmail.setFocusableInTouchMode(true);
+        etEmail.setErrorEnabled(true);
+        etEmail.setError("Don't input null content");
 //        Tab布局
-        TabLayout tab_desgin = (TabLayout) findViewById(R.id.tab_design);
-        tab_desgin.addTab(tab_desgin.newTab().setText("Tab One"));
-        tab_desgin.addTab(tab_desgin.newTab().setText("Tab Two"));
-        tab_desgin.addTab(tab_desgin.newTab().setText("Tab Three"));
+        TabLayout tab_design = (TabLayout) findViewById(R.id.tab_design);
+        tab_design.addTab(tab_design.newTab().setText("Tab One"));
+        tab_design.addTab(tab_design.newTab().setText("Tab Two"));
+        tab_design.addTab(tab_design.newTab().setText("Tab Three"));
 
 //        创建并得到FloatingActionButton
         final FloatingActionButton fab;
@@ -65,15 +94,49 @@ public class MainActivity extends AppCompatActivity {
                         .show();
             }
         });
-
-        EditText etPass = (EditText) findViewById(R.id.et_pass);
-
-        TextInputLayout etEmail = (TextInputLayout) findViewById(R.id.input_layout_email);
-//        下面前两个属性设置后不会自动聚焦
-        etEmail.setFocusable(true);
-        etEmail.setFocusableInTouchMode(true);
-        etEmail.setErrorEnabled(true);
-        etEmail.setError("Don't input null content");
-
     }
+
+    protected void setupToobBar() {
+//        利用Toolbar添加DrawerToggle
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    /**
+     * 重写返回键响应，先关闭抽屉再退出。
+     */
+    @Override
+    public void onBackPressed() {
+        if (isDrawerOpened()) {
+            mDrawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    //    判断抽屉是否打开
+    protected boolean isDrawerOpened() {
+        return mDrawer != null && mDrawer.isDrawerOpen(GravityCompat.START);
+    }
+
+    //    基本菜单项
+  /*  @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings : break;
+        }
+        return super.onOptionsItemSelected(item);
+    }*/
 }
